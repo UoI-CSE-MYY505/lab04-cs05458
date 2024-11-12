8893ae1ed3c42c4bdc45c700971368f7bc3ba9de
Georgethe134th <cs05458@uoi.gr>
2024-11-12 15:44:06 +0200

    Lab04 Completed

--- lab04.s
+++ lab04.s
@@ -30,7 +30,21 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
+            loop:
+                addi a0, a0,1
+                 addi a1, a1,1
+                #load the two current chars
+                lbu t0, -1(a0)
+                lbu t1, -1(a1)
+                bne t0,t1,diff
+                bne t0, zero, loop
+                ge:
+                addi a0, zero, 1
+                jr ra
+            diff:
+                bge t0, t1, ge
+                 add a0, zero, zero
+                jr   ra
  
 # ----------------------------------------------------------------------------
 # recCheck(array, size)
@@ -46,4 +60,30 @@ recCheck:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
+            addi t0,zero,2
+            bge a1,t0,cmp
+            addi a0, zero, 1
+            jr ra
+            cmp:
+                addi sp, sp,-12
+                sw ra, 8(sp)
+                sw a1, 4(sp)
+                sw a0, 0(sp)
+                lw a1, 0(a0)
+                lw a0, 4(a0) 
+                jal str_ge
+                bne a0,zero, next
+                add a0, zero,zero
+                lw ra, 8(sp)
+                addi sp, sp,12
+                jr ra
+                next: 
+                 lw a1, 4(sp)
+                 lw a0, 0(sp)
+                 addi sp, sp, 8
+                 addi a0, a0, 4
+                 addi a1, a1, -1
+                 jal recCheck
+                 lw ra, 0(sp)
+                 addi sp, sp, 4                 
+                 jr   ra
